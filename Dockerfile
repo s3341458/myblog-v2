c2a3daae627ee0b582b4ea5413be0e656f42f704
s3341458 <s3341458@student.rmit.edu.au>
2018-06-06 23:20:57 +1000

    make myblog use postgres container

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@
  WORKDIR /code
  ADD requirements.txt /code/
  RUN pip install -r requirements.txt
+ USER 1000:1000
  ADD . /code/
--- myblog/settings.py
+++ myblog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblog',
+        'USER': 'myblog',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
