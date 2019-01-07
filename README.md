274c6ae2134013726770aee0e235ba900f35f12b
chengyu <chengyu0316@gmail.com>
2019-01-08 10:12:48 +1100

    reset caddy settings

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # myblog-v2
 a upgraded version of my personal website [my personal website](https://yucheng.life "Cheng Homepage")
+
+ps: django part seems not needed at the moment since in this version my website is implemented as SPA,
+    I still keep django folder here in case I need it.
--- caddy/entrypoint.sh
+++ caddy/entrypoint.sh
@@ -6,13 +6,14 @@ $HOST_DOMAIN
 gzip
 log /var/log/caddy_access.log
 errors /var/log/caddy_errors.log
-proxy / $PROXY_DOMAIN
-ENDFILE
 
+root react_build
+
+ENDFILE
 
 if [[ $DEBUG = "True" ]]
 then
-    ./caddy
+    caddy
 else
     until curl $PROXY_DOMAIN; do
       >&2 echo "Service is unavailable - sleeping"
@@ -21,5 +22,5 @@ else
     # still do not exactly why but basically caddy started too early where
     # some network service is not really ready
     sleep 10
-    ./caddy -agree -email chengyu0316@gmail.com -host $HOST_DOMAIN -log stderr
+    caddy -agree -email chengyu0316@gmail.com -host $HOST_DOMAIN -log stderr
 fi
