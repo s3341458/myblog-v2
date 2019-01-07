f1199b518226f26d68766dbde79f33f42f5a2aa3
chengyu <chengyu0316@gmail.com>
2019-01-08 10:05:55 +1100

    remove the django project

--- myblog/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-import homepage.views as view
-
-
-urlpatterns = [
-    path('', view.IndexView.as_view(), name="index"),
-    path('blog', view.BlogView.as_view(), name="blog"),
-    path('timeline', view.TimelineView.as_view(), name="timeline"),
-]
