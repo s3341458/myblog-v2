5aa7ad907812920e3010336c6b6e316a252a22c8
chengyu <chengyu0316@gmail.com>
2019-02-10 09:50:37 +1100

    auto deploy sh set

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # update docker hub registry
-docker-compose build
-docker-compose push
+#docker-compose build
+#docker-compose push
 
-# redeploy
-hyper compose down
-hyper pull chengyu0316/myblog_v2
-hyper pull chengyu0316/caddy
-hyper compose up -f hyper.yml -d
+# deploy
+export DOCKER_TLS_VERIFY="1";
+export DOCKER_HOST="tcp://35.239.194.202:2376";
+export DOCKER_CERT_PATH="~/.docker/machine/machines/swarm-manager";
+export DOCKER_MACHINE_NAME="swarm-manager";
+docker stack deploy -c prod.yml pw
+
+# back to normal mode
+unset DOCKER_TLS_VERIFY "1";
+unset DOCKER_HOST "tcp://35.239.194.202:2376";
+unset DOCKER_CERT_PATH "/home/chengyu/.docker/machine/machines/swarm-manager";
+unset DOCKER_MACHINE_NAME "swarm-manager";
 
