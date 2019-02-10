85e0861752cc48ae144c0f7911845e046887a1a7
chengyu <chengyu0316@gmail.com>
2019-02-10 12:01:13 +1100

    fix deployment shell

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # update docker hub registry
-#docker-compose build
-#docker-compose push
+docker-compose build
+docker-compose push
 
 # deploy
-docker-swarm init
-export DOCKER_TLS_VERIFY="1";
-export DOCKER_HOST="tcp://35.239.194.202:2376";
-export DOCKER_CERT_PATH="~/.docker/machine/machines/swarm-manager";
-export DOCKER_MACHINE_NAME="swarm-manager";
+docker swarm init
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://35.239.194.202:2376"
+export DOCKER_CERT_PATH="~/.docker/machine/machines/swarm-manager"
+export DOCKER_MACHINE_NAME="swarm-manager"
 docker stack deploy -c prod.yml pw
 
 # back to normal mode
-docker-swarm leave --force
-unset DOCKER_TLS_VERIFY "1";
-unset DOCKER_HOST "tcp://35.239.194.202:2376";
-unset DOCKER_CERT_PATH "/home/chengyu/.docker/machine/machines/swarm-manager";
-unset DOCKER_MACHINE_NAME "swarm-manager";
+docker swarm leave --force
+unset DOCKER_TLS_VERIFY
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+unset DOCKER_MACHINE_NAME
 
