79601a359ee24a575731cb3084034c0562c60bf0
chengyu <chengyu0316@gmail.com>
2019-02-10 21:31:33 +1100

    do not how to leave swarm mode properly with the in same shell

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,7 @@ echo "deploy containers"
 docker stack deploy -c prod.yml pw
 
 echo "back to normal mode"
-docker swarm leave --force
+unset DOCKER_TLS_VERIFY
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+unset DOCKER_MACHINE_NAME
